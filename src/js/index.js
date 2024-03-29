55deb33af7b2656ee80aa06833917d7b607285b6
Keven Pereira <infokeven37@gmail.com>
2024-03-29 11:12:24 -0300

    ajuste
    Ajuste da imagens "sol" e "lua", correção no modo escuro.

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,14 @@ const body = document.querySelector('body');
 const imagemBotaoTrocaDeTema = document.querySelector('.imagem-botao');
 
 botaoAlterarTema.addEventListener("click", () =>{
-    const modoEscuroEstaAtivo =body.classList.contains("modo-escuro");
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
 
     body.classList.toggle("modo-escuro");
 
     if (modoEscuroEstaAtivo) {
-        imagemBotaoTrocaDeTema.setAttribute("src", ".src/img/sun.png");
+        imagemBotaoTrocaDeTema.setAttribute("src", "src/img/sun.png");
     
     } else {
-        imagemBotaoTrocaDeTema.setAttribute("src", ".src/img/moon.png")
+        imagemBotaoTrocaDeTema.setAttribute("src", "src/img/moon.png")
     }
 });
